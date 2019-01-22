26c487d7a8be6014722a395a424856bbb2be4307
Abbie <arvannoord@hotmail.com>
2019-01-22 18:30:26 -0500

    Finished Lift Class

--- robot/DeepSpace/src/main/cpp/Lifter.cpp
+++ robot/DeepSpace/src/main/cpp/Lifter.cpp
@@ -8,24 +8,34 @@
 #include "Lifter.h"
 #include "frc/WPILib.h"
 #include "ctre/Phoenix.h"
+#include <iostream>
 
 int const ENCODER_ID = 0;
-int const TICKS_PER_ROTATION = 4096;
+double const TICKS_PER_ROTATION = 4096;
 TalonSRX liftMotor = {ENCODER_ID};
-double levels [] = {0.0, 1.0, 3.5, 5.0, 6.4, 7.0, 10.0};
+double levels [] = {0.0, 1.0, 3.5, 5.0, 6.4, 7.0, 150.0};
 
-Lifter::Lifter() {
-}
+Lifter::Lifter() {}
 
 //this stuff is from Abbie and Clara's code
 
 void Lifter::RaiseLift(int level) 
 {
+    std::cout << "Raised lift to ";
+    std::cout << level <<std::endl;
+    std::cout << levels[level] << std::endl;
+    std::cout << TICKS_PER_ROTATION << std::endl;
+    std::cout << levels[level]*TICKS_PER_ROTATION << std::endl;
     liftMotor.Set(ControlMode::Position, levels[level]*TICKS_PER_ROTATION);
 }
 
 void Lifter::LiftInit()
 {
+    liftMotor.ConfigSelectedFeedbackSensor(FeedbackDevice::CTRE_MagEncoder_Relative);
+    liftMotor.SetSensorPhase(true);
+
+//    liftMotor.ConfigClosedLoopPeakOutput(0, .1);
+
     liftMotor.SetSelectedSensorPosition(0);
 }
 
