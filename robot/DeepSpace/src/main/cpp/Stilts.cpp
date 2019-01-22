578253ea8308066bcd05cb00134b8fc6f72978ae
Robert Lindskov <shadowshell17@gmail.com>
2019-01-22 18:26:44 -0500

    Added Stilts.cpp

--- robot/DeepSpace/src/main/cpp/Stilts.cpp
+++ robot/DeepSpace/src/main/cpp/Stilts.cpp
@@ -1,3 +1,8 @@
-#include "Stilts.h"
-#include "frc/WPILib.h"
-#include "ctre/Phoenix.h"
+#include <Stilts.h>
+#include <frc/WPILib.h>
+#include <ctre/Phoenix.h>
+
+Stilts::Stilts(TalonSRX& driveMotor, TalonSRX& backMotor, TalonSRX& frontMotor) : m_backMotor(backMotor), m_driveMotor(driveMotor), m_frontMotor(frontMotor)
+{
+
+}
--- robot/DeepSpace/src/main/include/Stilts.h
+++ robot/DeepSpace/src/main/include/Stilts.h
@@ -1,4 +1,4 @@
-#pragma
+#pragma once
 
 #include <frc/WPILib.h>
 #include <ctre/Phoenix.h>
@@ -6,9 +6,11 @@
 class Stilts
 {
     public:
-        Stilts();
-        void setFrontToHeight(float heigt);//Height in inches
+        Stilts(TalonSRX& driveMotor, TalonSRX& backMotor, TalonSRX& frontMotor);
+        void setFrontToHeight(float height);//Height in inches
         void setBackToHeight(float height);//Height in inches
         void driveWheels(float speed);//Speed from -1 to 1
-
-}
+        TalonSRX& m_driveMotor;
+        TalonSRX& m_backMotor;
+        TalonSRX& m_frontMotor;
+};
