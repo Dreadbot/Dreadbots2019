49f806280dfc2398bbdba99cc894af1f59b9794e
KyleGBC <kyle.roleson@dexterschools.org>
2019-01-26 10:08:59 -0500

    Did Stilts.cpp all good and encoders

--- robot/DeepSpace/src/main/cpp/Stilts.cpp
+++ robot/DeepSpace/src/main/cpp/Stilts.cpp
@@ -1,22 +1,22 @@
 #include <Stilts.h>
 #include <frc/WPILib.h>
 #include <ctre/Phoenix.h>
-
+float rotationsToInches = 123;
 Stilts::Stilts(TalonSRX& driveMotor, TalonSRX& backMotor, TalonSRX& frontMotor) : m_backMotor(backMotor), m_driveMotor(driveMotor), m_frontMotor(frontMotor)
 {
 
 }
 void Stilts::setFrontToHeight(float height)
 {
-
+    m_frontMotor.Set(ControlMode::Position, height*rotationsToInches);
 }
 
 void Stilts::setBackToHeight(float height)
 {
-
+    m_backMotor.Set(ControlMode::Position, height*rotationsToInches);
 }
 
 void Stilts::driveWheels(float speed)
 {
-    
+    m_driveMotor.Set(ControlMode::PercentOutput, speed);
 }
