80df3ed5df3b8318671ac19c1e917c24d77f102d
Cole Scheller <schelcc@pobox.com>
2019-01-30 11:30:28 -0500

    Succesfully called grip pipeline

--- vision/stickynote.py
+++ vision/stickynote.py
@@ -112,7 +112,7 @@ class GripPipeline:
         else:
             mode = cv2.RETR_LIST
         method = cv2.CHAIN_APPROX_SIMPLE
-        im2, contours, hierarchy =cv2.findContours(input, mode=mode, method=method)
+        contours, hierarchy =cv2.findContours(input, mode=mode, method=method)
         return contours
 
     @staticmethod
--- vision/testpipeline.py
+++ vision/testpipeline.py
@@ -1,67 +1,26 @@
-#!/usr/bin/env python
-
-from __future__ import print_function
-
-import io
-import time
-#import picamera
+import numpy as np
 import cv2
-from imutils.video import VideoStream
 import stickynote
 
+cap = cv2.VideoCapture(0)
+grip = stickynote.GripPipeline()
 
-"""
-Demonstates continous image capture technique suggested here:
- - http://www.pyimagesearch.com/2015/03/30/accessing-the-raspberry-pi-camera-with-opencv-and-python/
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
 
-"""
+    # Our operations on the frame come here
 
-class Timer(object):
-    def __init__(self, name):
-        self.name = name
-        self.t1 = time.time()
-        pass
+    grip.process(frame)
+    
 
-    def start(self):
-        self.t1 = time.time()
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-    def stop(self):
-        self.t2 = time.time()
+    
 
-    def elapsed(self):
-        return self.t2-self.t1
-
-    def show(self):
-        print( "%s: %4.4f" % ( self.name, (self.t2-self.t1) ) )
-#|
-
-t = Timer( "Camera continous" )
-idx=0
-cam = VideoStream(src=1).start()
-
-t.start()
-#cam = cv2.VideoCapture(0)
-#stream = io.BytesIO()
-
-for idx in range(20):
-    img = cam.read()
-
-t.stop()
-
-cv2.imshow( 'frame', img )
-cv2.waitKey(0)
-
-
-pipe = stickynote.GripPipeline()
-pipe.process(img)
-
-print(pipe.filter_contours_output())
-
-print( "%s iterations of image captures" % idx )
-
-secs = (t.t2 - t.t1)
-fps = 1.0 * idx / secs
-
-t.show()
-
-print( "fps = %.2f" % (fps) )
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
